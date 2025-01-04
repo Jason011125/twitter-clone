ed37f7df4da8eca429a3c94135b864df95208bee
Jason011125 <zic022@ucsd.edu>
2025-01-04 13:09:58 -0800

    auth handled and routes protected in client.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:5002
-  }
-})
+  server: {
+    port: 5002,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5001",
+        changeOrigin: true,
+      },
+    },
+  },
+});
