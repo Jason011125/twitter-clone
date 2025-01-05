6179dd63646239a0e4f2da93d1236070be595ffc
Jason011125 <zic022@ucsd.edu>
2025-01-05 15:18:33 -0800

    suggested users fetched

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT||5001;
 
-app.use(express.json());
+app.use(express.json({limit:"10mb"}));
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 app.use(cookieParser());
 
